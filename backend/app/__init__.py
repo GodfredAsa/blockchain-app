eeb6a41b6da4d0acdc4aca4078309cb23d6f9730
GodfredAsa <godfred.asumadu.asamoah@gmail.com>
2023-07-01 03:34:52 +0000

    implements synchronizing peer on startup

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,8 +4,8 @@ from flask import Flask, jsonify
 from typing import List
 from backend.blockchain.blockchain import Blockchain
 from backend.blockchain.block import Block
-import  http.client as status
-
+import http.client as status
+import requests
 from backend.pubsub import PubSub
 
 app = Flask(__name__)
@@ -22,7 +22,7 @@ def route_default():
 @app.route('/blockchain')
 def route_blockchain() -> List['Block']:
     print(blockchain)
-    return jsonify(blockchain.to_json()), status.OK
+    return jsonify(blockchain.to_json())
 
 
 @app.route('/blockchain/mine')
@@ -37,9 +37,21 @@ def route_blockchain_mine():
 # SETTING RANDOM PORTS BETWEEN 5001-5999. BELOW IS THE COMMAND FOR RUNNING ANOTHER INSTANCE 
 # export PEER=True && python3 -m backend.app
 
-PORT = 5000
+ROOT_PORT = 5000
+PORT = ROOT_PORT
+
 if os.environ.get('PEER') == 'True':
     PORT = random.randint(5001, 6000)
+    results = requests.get(f'http://127.0.0.1:{ROOT_PORT}/blockchain')
+    print(f'Results JSON => {results.json()}')
+    result_blockchain: 'Blockchain' = Blockchain.from_json(results.json())
+
+    try:
+        blockchain.replace_chain(result_blockchain.chain)
+        print('In -- Successfully synchronized the local chain')
+    except Exception as e:
+        print(f'Couldn\'t Successfully Synchronized Chain, {str(e)}')
+
 
 app.run(port=PORT)
 # if __name__ == '__main_-':
